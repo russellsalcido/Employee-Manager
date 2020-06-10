b5540fe587e8c254a72a3d626f22672cf8c14b64
Russell Salcido <russell.salcido@gmail.com>
2020-06-09 20:25:59 -0700

    masterclass for shared data properties

---
+++ js/master-class.js
@@ -0,0 +1,15 @@
+class X {
+    constructor(x, x, x) {
+      this.id = id;
+      this.numberOfWheels = numberOfWheels;
+      this.sound = sound;
+    }
+  
+    printInfo() {
+      console.log(`This vehicle has ${this.X} wheels`);
+      console.log(`This vehicle has an id of ${this.id}`);
+    }
+  }
+  module.exports = X;
+
+  
