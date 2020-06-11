aae05ffad55adb870813cedd60460b3c008c4d6d
Russell Salcido <russell.salcido@gmail.com>
2020-06-10 22:07:58 -0700

    built seed template for 3 tabls

--- db/schemaET.sql
+++ db/schemaET.sql
@@ -3,26 +3,27 @@ CREATE DATABASE employeeTrackerDB;
 
 USE employeeTrackerDB;
 
-CREATE TABLE employee (
-  id INT,
-  first_name VARCHAR(30) NOT NULL, -- to hold employee first name
-  last_name VARCHAR(30) NOT NULL, -- to hold employee last name
-  role_id INT, -- to hold reference to role employee has
-  manager_id INT NULL, -- to hold reference to another employee that is the manager of the current employee. This field may be null if the employee has no manager
-  PRIMARY KEY (id)
-);
 
 CREATE TABLE department (
-  id INT,
+  id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(30), -- to hold department name
   PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-  id INT, 
+  id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL, -- to hold role title
-  salary DECIMAL (10,4) NOT NULL, -- to hold role salary
+  salary DECIMAL (10,2) NOT NULL, -- to hold role salary
   department_id INT, -- to hold reference to department role belongs to
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL, -- to hold employee first name
+  last_name VARCHAR(30) NOT NULL, -- to hold employee last name
+  role_id NOT NULL AUTO_INCREMENT, -- to hold reference to role employee has
+  manager_id INT NULL AUTO_INCREMENT, -- to hold reference to another employee that is the manager of the current employee. This field may be null if the employee has no manager
   PRIMARY KEY (id)
 );
 
---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+USE employeeTrackerDB;
+
+INSERT INTO department (name)
+VALUES ("");
+
+USE employeeTrackerDB;
+
+INSERT INTO role (title)
+VALUES ("");
+
+INSERT INTO role (salary)
+VALUES ("");
+
+USE employeeTrackerDB;
+
+INSERT INTO employee (first_name)
+VALUES ("");
+
+INSERT INTO employee (last_name)
+VALUES ("");
+
+INSERT INTO employee (first_name)
+VALUES ("");
