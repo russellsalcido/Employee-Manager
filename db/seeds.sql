ebb95709210e72b70c2bc740bb47221617ef7715
Russell Salcido <russell.salcido@gmail.com>
2020-07-05 16:36:56 -0700

    updated seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,23 +1,15 @@
-USE employeeTrackerDB;
-
+USE trackerDB;
 INSERT INTO department (name)
-VALUES ("");
+VALUES ("Accounting"), ("Engineering"),("Toilets");
 
-USE employeeTrackerDB;
+INSERT INTO role (title, salary, department_id)
+VALUES ("Chief Accountant", 100000, 1),
+       ("Accountant", 70000, 1),
+       ("Lead Engineer", 125000, 2),
+       ("Engineer", 80000, 2);
 
-INSERT INTO role (title)
-VALUES ("");
-
-INSERT INTO role (salary)
-VALUES ("");
-
-USE employeeTrackerDB;
-
-INSERT INTO employee (first_name)
-VALUES ("");
-
-INSERT INTO employee (last_name)
-VALUES ("");
-
-INSERT INTO employee (first_name)
-VALUES ("");
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Bobert", "Roberson", 1, null), 
+        ("Greedy", "McNasty", 2, 1),
+        ("Flo", "Downs", 3, null),
+        ("Salty", "Neversmile", 4, 3)
